4e233a755ba77be7c7ddf6b3c057a9775a226733
cemile871 <250541099@firat.edu.tr>
2025-12-03 19:34:21 +0300

    Update Exercise4_1.java

--- Exercise4_1.java
+++ Exercise4_1.java
@@ -10,5 +10,20 @@ public class Exercise4_1 {
 
     public static void main(String[] args) {
         // TODO: test methods
+        String day = "Sunday";
+        String month = "July";
+        int date = 14;
+        int year = 2016;
+
+        printAmerican (day, date, month, year);
+        printEuropean (day, date, month, year); 
+
+        System.out.println("Amerikan Formatı:");
+        System.out.println(day + ", " + month + " " + date + ", " + year);
+        System.out.println();
+        System.out.println("Avrupa Formatı:");
+        System.out.println(date + " " + month + " " + year + ", " + day);
+ 
+            
     }
 }
